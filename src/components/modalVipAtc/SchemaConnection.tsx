c40e755c579d888610e45bf05defee38cc02bec6
Ivan Bondarev <bondarev-i@tl.protei.ru>
2025-10-28 15:49:16 +0300

    add images and update VIP page with modal functionality

---
+++ src/components/modalVipAtc/SchemaConnection.tsx
@@ -0,0 +1,7 @@
+export const SchemaConnection = () => {
+  return (
+    <div className='absolute top-0 left-0 w-full h-full'>
+      <img className='w-screen' src='/images/modalVipAtcBody.png' alt='' />
+    </div>
+  );
+};
