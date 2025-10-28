eb7e994bb1be324d84b3a6f400ca1990d1174504
qwerty63 <st.daniil.haritonov@gmail.com>
2025-10-28 13:55:09 +0300

    ATCSmallCommit

--- src/pages/ATCSmall.tsx
+++ src/pages/ATCSmall.tsx
@@ -1,8 +1,19 @@
 export const ATCSmall = () => {
-  return (
-    <div className='p-8'>
-      <h1 className='text-2xl font-bold mb-4'>АТС малой ёмкости</h1>
-      <p>Содержимое страницы АТС малой ёмкости</p>
-    </div>
-  );
+    return (
+        <div className='pl-[200px] pt-[143px] pb-[244px] inline-flex flex-row '>
+            <div className='bg-deep-blue h-[1600px] rounded-[60px]'>
+                <p className='text-large leading-[100%] text-white text-center mt-[46px] mb-[57px]'>
+                    Телефонизация на примере<br />административного здания
+                </p>
+                <img className='w-[1472px]' src='/images/ATCSmall.png' alt=''/>
+            </div>
+            <div className='flex flex-col gap-[7px] items-start z-100 ml-[123px]'>
+                <p className='text-extraBig font-bold leading-[100%]'>
+                    АТС малой ёмкости
+                </p>
+                <p className='text-extra leading-[100%]'>До 1 000 абонентов</p>
+
+            </div>
+        </div>
+    );
 };
