ee2659bfe2f7910408f874fcc76416c7ef0101ed
qwerty63 <st.daniil.haritonov@gmail.com>
2025-10-28 13:11:55 +0300

    WhatsAtcCommit

---
+++ src/components/modalWhatATC/WhatATCModal.tsx
@@ -0,0 +1,95 @@
+export const WhatATCModal = () => {
+    return (
+        <div>
+            <div
+                className='w-[1319px] bg-deep-blue top-0 left-1/2 transform -translate-x-1/2 absolute flex items-center justify-center pt-[30px] pb-[43px] text-extra text-white leading-[1] rounded-b-[60px]'>
+                Как это поможет вашему бизнесу?
+            </div>
+            <div className='flex items-start mt-[117px] justify-center'>
+                <div className='flex items-center justify-start mt-[82px] m-[15px]'>
+                    <div className='flex'>
+                        <img className='w-[78px] h-[78px] mr-[40px]' src='/images/CheckCircleOutlined.png' alt=''/>
+                        <div className='w-[596px]'>
+                            <div className='text-medium font-semibold'>
+                                Вы предоставите удобный инструмент коммуникации вашим сотрудникам
+                            </div>
+                            <div className='text-medium mt-[15px]'>
+                                Создавайте единое коммуникационное пространство: совмещайте звонки, видеоконференции и
+                                совместную работу над документами в один технологический контур. Это даёт возможность
+                                удерживать фокус на критически важных проектах и сокращать сроки принятия решений.
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className='flex items-center justify-start mt-[82px] m-[15px]'>
+                    <div className='flex'>
+                        <img className='w-[78px] h-[78px] mr-[40px]' src='/images/CheckCircleOutlined.png' alt=''/>
+                        <div className='w-[596px]'>
+                            <div className='text-medium font-semibold'>
+                                Вы будете на шаг впереди
+                            </div>
+                            <div className='text-medium mt-[15px]'>
+                                Переосмыслите свой опыт взаимодействия с АТС нового поколения,
+                                специально разработанной для развивающихся бизнесов сегодня и в
+                                будущем. Наша команда обеспечивает бесперебойное обслуживание
+                                вашей АТС, что позволяет вам быть лидером в отрасли.
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className='flex items-center justify-start mt-[82px] m-[15px]'>
+                    <div className='flex'>
+                        <img className='w-[78px] h-[78px] mr-[40px]' src='/images/CheckCircleOutlined.png' alt=''/>
+                        <div className='w-[596px]'>
+                            <div className='text-medium font-semibold'>
+                                Вы минимизируете издержки при масштабировании сети связи
+                            </div>
+                            <div className='text-medium mt-[15px]'>
+                                Система растёт вместе с вашей компанией: при открытии новых
+                                производственных участков, заводов или офисов интегрируйте их
+                                в общую телефонную сеть без потери качества и функционала.
+                                Это гарантия стабильного роста без сложных изменений в
+                                инфраструктуре и излишних затрат.
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className='flex items-start justify-center'>
+                <div className='flex items-center justify-start mt-[27px] m-[15px]'>
+                    <div className='flex'>
+                        <img className='w-[78px] h-[78px] mr-[40px]' src='/images/CheckCircleOutlined.png' alt=''/>
+                        <div className='w-[596px]'>
+                            <div className='text-medium font-semibold'>
+                                Вы повысите операционную эффективность
+                            </div>
+                            <div className='text-medium mt-[15px]'>
+                                Чётко выстроенная система связи — это не только слаженное
+                                взаимодействие между отделами, но и снижение простоев в
+                                производственных процессах. В итоге вы экономите ресурсы
+                                и ускоряете принятие решений даже в многотысячном коллективе.
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className='flex items-center justify-start mt-[27px] m-[15px]'>
+                    <div className='flex'>
+                        <img className='w-[78px] h-[78px] mr-[40px]' src='/images/CheckCircleOutlined.png' alt=''/>
+                        <div className='w-[596px]'>
+                            <div className='text-medium font-semibold'>
+                                Вы устраните риски утечки информации
+                            </div>
+                            <div className='text-medium mt-[15px]'>
+                                Обеспечьте безопасность корпоративных переговоров с
+                                помощью комплексных мер защиты информации. В результате
+                                компания нивелирует финансовые риски.
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
--- src/pages/WhatATC.tsx
+++ src/pages/WhatATC.tsx
@@ -1,100 +1,87 @@
 import {Modal} from "../components/modal/Modal.tsx";
 import {useState} from "react";
+import {WhatATCModal} from "../components/modalWhatATC/WhatATCModal.tsx";
 
 export const WhatATC = () => {
     const [isModalOpen, setIsModalOpen] = useState(false);
 
-  return (
-    <div className='p-8'>
-        <p className='mt-[107px] font-bold leading-[100%] text-[128px] text-center'>
-            АТС НОВОГО ПОКОЛЕНИЯ — ЭТО:
-        </p>
-        <div className='mt-[27px] flex justify-center'>
-            <div className='w-[1020px] m-[100px]'>
-                <img className='size-[182px] m-auto' src='/images/CheckCircleOutlined.png' alt='' />
-                <p className='text-extra text-deep-blue mt-[30px] text-center'>
-                    Стабильность, которой можно<br></br>
-                    доверять
-                </p>
-                <p className='text-5xl mt-[30px] leading-[120%]'>
-                    Создайте единую среду корпоративных<br></br>
-                    коммуникаций, рассчитанную на<br></br>
-                    высокие абонентские нагрузки и<br></br>
-                    распределённую инфраструктуру. Наша<br></br>
-                    АТС нового поколения обеспечит<br></br>
-                    предсказуемую производительность в<br></br>
-                    долгосрочной перспективе.
-                </p>
-            </div>
+    return (
+        <div className='p-8'>
+            <p className='mt-[107px] font-bold leading-[100%] text-[128px] text-center'>
+                АТС НОВОГО ПОКОЛЕНИЯ — ЭТО:
+            </p>
+            <div className='mt-[27px] flex justify-center'>
+                <div className='w-[1020px] m-[100px]'>
+                    <img className='size-[182px] m-auto' src='/images/CheckCircleOutlined.png' alt=''/>
+                    <p className='text-extra text-deep-blue mt-[30px] text-center'>
+                        Стабильность, которой можно<br></br>
+                        доверять
+                    </p>
+                    <p className='text-5xl mt-[30px] leading-[120%]'>
+                        Создайте единую среду корпоративных<br></br>
+                        коммуникаций, рассчитанную на<br></br>
+                        высокие абонентские нагрузки и<br></br>
+                        распределённую инфраструктуру. Наша<br></br>
+                        АТС нового поколения обеспечит<br></br>
+                        предсказуемую производительность в<br></br>
+                        долгосрочной перспективе.
+                    </p>
+                </div>
 
-            <div className='w-[1020px] m-[100px]  mb-0'>
-                <img className='size-[182px] m-auto' src='/images/CheckCircleOutlined.png' alt='' />
-                <p className='text-extra text-deep-blue mt-[30px] text-center'>
-                    Омниканальная связь для<br></br>
-                    предприятий любого<br></br>
-                    масштаба
-                </p>
-                <p className='text-5xl mt-[30px] leading-[120%]'>
-                    Собирайте воедино голосовую связь,<br></br>
-                    чаты и видеоконференции, чтобы<br></br>
-                    сократить затраты на коммуникации и<br></br>
-                    повысить эффективность<br></br>
-                    межфилиального взаимодействия. Это<br></br>
-                    особенно актуально для холдингов с<br></br>
-                    широкой географией присутствия, где<br></br>
-                    требуется безупречная слаженность<br></br>
-                    между офисами, производственными<br></br>
-                    площадками или удаленными<br></br>
-                    подразделениями.
-                </p>
-            </div>
+                <div className='w-[1020px] m-[100px]  mb-0'>
+                    <img className='size-[182px] m-auto' src='/images/CheckCircleOutlined.png' alt=''/>
+                    <p className='text-extra text-deep-blue mt-[30px] text-center'>
+                        Омниканальная связь для<br></br>
+                        предприятий любого<br></br>
+                        масштаба
+                    </p>
+                    <p className='text-5xl mt-[30px] leading-[120%]'>
+                        Собирайте воедино голосовую связь,<br></br>
+                        чаты и видеоконференции, чтобы<br></br>
+                        сократить затраты на коммуникации и<br></br>
+                        повысить эффективность<br></br>
+                        межфилиального взаимодействия. Это<br></br>
+                        особенно актуально для холдингов с<br></br>
+                        широкой географией присутствия, где<br></br>
+                        требуется безупречная слаженность<br></br>
+                        между офисами, производственными<br></br>
+                        площадками или удаленными<br></br>
+                        подразделениями.
+                    </p>
+                </div>
 
-            <div className='w-[1020px] m-[100px]  mb-0'>
-                <img className='size-[182px] m-auto' src='/images/CheckCircleOutlined.png' alt='' />
-                <p className='text-extra text-deep-blue mt-[30px] text-center'>
-                    Гибкость в одном<br></br>
-                    решении
-                </p>
-                <p className='text-5xl mt-[30px] leading-[120%]'>
-                    Разнообразные модули в составе АТС<br></br>
-                    нового поколения покрывают весь<br></br>
-                    спектр задач: от телефонизации<br></br>
-                    офисов до оснащения оперативной<br></br>
-                    диспетчерской связью<br></br>
-                    производственные площадки.
-                </p>
-            </div>
-        </div>
-
-        <div onClick={() => setIsModalOpen(true)} className='mr-[150px] border-2 border-black rounded-[60px] w-[1050px] h-[240px] float-right flex justify-between p-[45px]'>
-            <p className='text-extra font-medium justify-center'>Как это поможет<br></br>вашему бизнесу?</p>
-            <img className='h-[114px] w-[70px] pt-[15px] pb-[15px] mr-[80px]' src='/images/round_right.png' alt='' />
-        </div>
-
-        {/* Модальное окно */}
-        <Modal
-            isOpen={isModalOpen}
-            onClose={() => setIsModalOpen(false)}
-            width='w-[2530px]'
-            height='h-[1424px]'
-        >
-            <div className='w-[1319px] bg-deep-blue top-0 left-1/2 transform -translate-x-1/2 absolute flex items-center justify-center pt-[30px] pb-[43px] text-extra text-white leading-[1] rounded-b-[60px]'>
-                Как это поможет вашему бизнесу?
-            </div>
-            <div className='w-[2330px] h-[984px] flex items-center justify-start mt-[117px]'>
-                <div className='flex'>
-                    <img className='w-[78px] h-[78px]' src='/images/CheckCircleOutlined.png' alt='' />
-                    <div className='w-[596px]'>
-                        <div>
-                            Вы предоставите удобный инструмент коммуникации вашим сотрудникам
-                        </div>
-                        <div>
-                            Создавайте единое коммуникационное пространство: совмещайте звонки, видеоконференции и совместную работу над документами в один технологический контур. Это даёт возможность удерживать фокус на критически важных проектах и сокращать сроки принятия решений.
-                        </div>
-                    </div>
+                <div className='w-[1020px] m-[100px]  mb-0'>
+                    <img className='size-[182px] m-auto' src='/images/CheckCircleOutlined.png' alt=''/>
+                    <p className='text-extra text-deep-blue mt-[30px] text-center'>
+                        Гибкость в одном<br></br>
+                        решении
+                    </p>
+                    <p className='text-5xl mt-[30px] leading-[120%]'>
+                        Разнообразные модули в составе АТС<br></br>
+                        нового поколения покрывают весь<br></br>
+                        спектр задач: от телефонизации<br></br>
+                        офисов до оснащения оперативной<br></br>
+                        диспетчерской связью<br></br>
+                        производственные площадки.
+                    </p>
                 </div>
             </div>
-        </Modal>
-    </div>
-  );
+
+            <div onClick={() => setIsModalOpen(true)}
+                 className='mr-[150px] border-2 border-black rounded-[60px] w-[1050px] h-[240px] float-right flex justify-between p-[45px]'>
+                <p className='text-extra font-medium justify-center'>Как это поможет<br></br>вашему бизнесу?</p>
+                <img className='h-[114px] w-[70px] pt-[15px] pb-[15px] mr-[80px]' src='/images/round_right.png' alt=''/>
+            </div>
+
+            {/* Модальное окно */}
+            <Modal
+                isOpen={isModalOpen}
+                onClose={() => setIsModalOpen(false)}
+                width='w-[2530px]'
+                height='h-[1424px]'
+            >
+                <WhatATCModal/>
+            </Modal>
+        </div>
+    );
 };
