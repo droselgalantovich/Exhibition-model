7c20f87eea1aa5afc0bc7b42bc781b17b83ba3fb
qwerty63 <st.daniil.haritonov@gmail.com>
2025-10-30 13:16:37 +0300

    ServerModalCommit

---
+++ src/components/modalServers/ThirdServerModal.tsx
@@ -0,0 +1,13 @@
+export const ThirdServerModal = () => {
+    return (
+        <div className='flex flex-col'>
+            <div
+                className='w-[531px] font-semibold bg-deep-blue top-0 left-1/2 transform -translate-x-1/2 absolute flex items-center justify-center  pt-[30px] pb-[30px] text-medium text-white leading-[1]'>
+                Server 3
+            </div>
+            <div className='absolute bg-deep-blue h-[65px] w-[60px] mt-[80px]'></div>
+            <div className='absolute bg-deep-blue h-[65px] w-[60px] mt-[80px] ml-[470px]'></div>
+            <img className='absolute w-[527px] h-[360px] mt-[93px] ml-[1px]' src='/images/ThirdServerFrame.png' alt=''/>
+        </div>
+    );
+};
